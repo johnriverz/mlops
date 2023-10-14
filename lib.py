0b78407d5d18303d7fd4db55f334e1a5dfc2e44e
johnriverz <juan.jose.rios.jr@gmail.com>
2023-10-13 18:05:35 -0700

    test

--- lib.py
+++ lib.py
@@ -4,3 +4,4 @@ import sklearn
 sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
